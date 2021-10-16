aeb4ceea7d6b537f7c0511840ce3f0d8b21a5d6d
Taft Miller <taftmiller@Tafts-MacBook-Pro.local>
2021-10-15 20:23:06 -0600

    5th commit added JS

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ function randomID() {
     return (Math.random() + Math.random()) * 1000;
 }
 
-
-
 function createList() {
     let id = randomID();
     let listName = document.getElementById("listCreation").value;
@@ -19,4 +17,32 @@ function createList() {
         })
         console.log(allLists)
     }
+    localStorageSave();
+    render();
 }
+
+function render() {
+    let listHTML = "";
+    allLists.forEach(list => {
+        listHTML += `
+            <div class="eachList">
+                <div class="listName">${list.listName}</div>
+            </div>
+        `
+    });
+    document.getElementById("listOfLists").innerHTML = listHTML;
+}
+
+function localStorageSave() {
+    localStorage.setItem("listsArray", JSON.stringify(allLists));
+}
+
+function localStorageBringBack() {
+    let todoData = JSON.parse(localStorage.getItem("listsArray"));
+    if (todoData !== null) {
+        allLists = todoData;
+    }
+    render();
+}
+
+localStorageBringBack();
